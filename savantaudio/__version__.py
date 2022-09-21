e3168365a5f1c3d5d523a3e75bd2d99c3cdd0c2b
Adam Kropp <30504149+akropp@users.noreply.github.com>
2022-09-21 11:03:23 -0400

    Update publish workflow

--- savantaudio/__version__.py
+++ savantaudio/__version__.py
@@ -7,7 +7,7 @@ Information about the current version of the savantaudio-client package.
 
 __title__ = 'savantaudio-client'
 __description__ = 'savantaudio-client - Savant Audio Switch Client'
-__version__ = '0.1.17'
+__version__ = '0.1.18'
 __author__ = 'Adam Kropp'
 __author_email__ = 'adam_kropp@yahoo.com'
 __license__ = 'Apache 2.0'
